06a0d0f15f2d1267d38072b58e4375a6cd0b1c11
Pansevich Vitali <skynet323@mail.ru>
2014-06-24 22:11:48 +0300

    del temp

--- weather.sh
+++ weather.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-time=11;
 while (true) do
     curl -s http://www.gismeteo.by/weather-minsk-4248/ > pogoda
     grep -m 1 "<dd class='value m_temp c'>" pogoda > temp
     echo  $(sed 's/[^\-\+0-9]//g' temp)
     rm temp;
     rm pogoda;
-    sleep $time
+    sleep 5
 done
