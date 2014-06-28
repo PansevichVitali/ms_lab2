81c79dbca4ea64eda942b9886875ad7939abc862
Pansevich Vitali <skynet323@mail.ru>
2014-06-28 23:31:11 +0300

    coding style

--- weather.sh
+++ weather.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while (true) do
-    curl -s http://www.gismeteo.by/weather-minsk-4248/ > pogoda
-    grep -m 1 'value m_temp c' pogoda > temp
-    echo  $(sed 's/[^\-\+0-9]//g' temp)
-    rm temp;
-    rm pogoda;
-    sleep 5
+	curl -s http://www.gismeteo.by/weather-minsk-4248/ > pogoda
+	grep -m 1 'value m_temp c' pogoda > temp
+	echo  $(sed 's/[^\-\+0-9]//g' temp)
+	rm temp;
+	rm pogoda;
+	sleep 5
 done
